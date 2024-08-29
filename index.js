6275f25daff98ca847f2be1b9b9c2fba09667b78
Hanru Vorster <hanru.vorster@gmail.com>
2024-08-29 11:05:47 +0200

    Created the instance and class

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+let bankBranchInstance = null;
 
+class BankBranch {
+  constructor(branchInfo) {
+    if (!bankBranchInstance) {
+      this.branchInfo = branchInfo;
+      bankBranchInstance = this;
+      Object.freeze(bankBranchInstance);
+    }
+    return bankBranchInstance;
+  }
+  getBranchInfo() {
+    console.log(`Branch Info: ${this.branchInfo}`);
+  }
+}
